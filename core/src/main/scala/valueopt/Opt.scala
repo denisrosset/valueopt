78ed5cf1405a3628d3dbc06e7e0a344fd61cc99c
Denis Rosset <physics@denisrosset.com>
2017-01-03 02:41:43 +0800

    Added (cargo-cult?) @inline final annotations.

--- core/src/main/scala/valueopt/Opt.scala
+++ core/src/main/scala/valueopt/Opt.scala
@@ -2,23 +2,23 @@ package valueopt
 
 object Opt extends OptVersions {
 
-  def apply[A](a: A): Opt[A] = new Opt(a)
+  @inline final def apply[A](a: A): Opt[A] = new Opt(a)
 
-  @inline def empty[A]: Opt[A] = new Opt[A](null.asInstanceOf[A])
+  @inline final def empty[A]: Opt[A] = new Opt[A](null.asInstanceOf[A])
 
 }
 
 class Opt[+A](val ref: A) extends OptVersions.Base {
-  def scala2_10hashCode: Int = ref.hashCode
-  def scala2_10equals(other: Any): Boolean = other match {
+  @inline final def scala2_10hashCode: Int = ref.hashCode
+  @inline final def scala2_10equals(other: Any): Boolean = other match {
     case that: Opt[_] => ref == that.ref
     case _ => false
   }
-  def isDefined: Boolean = ref != null
-  def nonEmpty: Boolean = ref != null
-  def isEmpty: Boolean = ref == null
+  @inline final def isDefined: Boolean = ref != null
+  @inline final def nonEmpty: Boolean = ref != null
+  @inline final def isEmpty: Boolean = ref == null
 
-  def get: A = {
+  @inline final def get: A = {
     ref.getClass // to throw NullPointerException early
     ref
   }
